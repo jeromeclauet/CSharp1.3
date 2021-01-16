c971c8eb61084892220d6f757a27816f690205d0
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 10:58:32 +0100

    Exercice 4

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v1;
+            string v2;
+            int firstNumber;
+
+            v1 = "Monsieur";
+            v2 = "Vincent";
+            firstNumber = 1;
+
+            Console.WriteLine("Bonjour " + v1 + " " + v2 + ", vous êtes venu nous rendre visite " + firstNumber + " fois" );
+        }
+    }
+}
